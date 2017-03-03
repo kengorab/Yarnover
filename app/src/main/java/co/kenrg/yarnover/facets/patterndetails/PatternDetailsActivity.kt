cd4641c18fccd3d2169bf29ae7ca1455914ba320
Ken Gorab <ken.gorab@gmail.com>
2017-03-02 20:03:48 -0500

    Removing Anko
    Don't use Anko on PatternDetailsActivity (even though the activity wasn't used at all yet anyway)

--- app/src/main/java/co/kenrg/yarnover/facets/patterndetails/PatternDetailsActivity.kt
+++ app/src/main/java/co/kenrg/yarnover/facets/patterndetails/PatternDetailsActivity.kt
@@ -1,52 +1,20 @@
 package co.kenrg.yarnover.facets.patterndetails
 
 import android.os.Bundle
-import android.support.design.widget.AppBarLayout
-import android.support.design.widget.AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
-import android.support.design.widget.AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL
-import android.support.v4.content.ContextCompat.getColor
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import co.kenrg.yarnover.R
-import co.kenrg.yarnover.ext.actionBarSize
-import org.jetbrains.anko.UI
-import org.jetbrains.anko.appcompat.v7.toolbar
-import org.jetbrains.anko.backgroundColor
-import org.jetbrains.anko.design.appBarLayout
-import org.jetbrains.anko.design.coordinatorLayout
-import org.jetbrains.anko.frameLayout
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.wrapContent
+import kotlinx.android.synthetic.main.activity_patterndetails.*
 
 class PatternDetailsActivity : AppCompatActivity() {
-
-  private fun makeLayout(activity: PatternDetailsActivity): View =
-      activity.UI {
-        coordinatorLayout {
-          fitsSystemWindows = true
-
-          appBarLayout {
-            lparams(width = matchParent, height = wrapContent)
-
-            toolbar(R.style.AppTheme_PopupOverlay) {
-              activity.setSupportActionBar(this)
-              activity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
-            }.lparams(width = matchParent, height = actionBarSize()) {
-              scrollFlags = SCROLL_FLAG_SCROLL or SCROLL_FLAG_ENTER_ALWAYS
-            }
-          }
-
-          frameLayout {
-            backgroundColor = getColor(activity, R.color.backgroundColor)
-
-          }.lparams(width = matchParent, height = matchParent) {
-            behavior = AppBarLayout.ScrollingViewBehavior()
-          }
-        }
-      }.view
+  private val activity: PatternDetailsActivity = this
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(makeLayout(this))
+    setContentView(R.layout.activity_patterndetails)
+
+    toolbar.apply {
+      activity.setSupportActionBar(this)
+      activity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+    }
   }
 }
